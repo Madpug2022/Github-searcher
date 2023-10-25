2e6de3f0bbbc5bc27218a77e39f6af119cd46eef
Matias Alaimo <matias.chiappa@hotmail.com>
2023-10-25 12:43:47 +0200

    Feat:(Added 404 page and finished css)

--- src/components/Repositories.tsx
+++ src/components/Repositories.tsx
@@ -1,4 +1,5 @@
 import RepositoryCard from "./RepositoryCard";
+import { useEffect, useState } from 'react'
 
 interface PropType {
     filter: string;
@@ -13,14 +14,30 @@ interface Repository {
 
 const Repositories = (props: PropType) => {
     const { filter, repositories } = props;
-    console.log(filter, repositories);
-    return (
-        <section
+    const [filteredRepositories, setFiltered] = useState<Repository[]>([])
+
+    useEffect(() => {
+        const filteredItems = repositories?.filter(item => item.name.includes(filter));
+        if (filteredItems) {
+            setFiltered(filteredItems)
+        }
+
+    }, [filter])
+    return (<>
+        {filteredRepositories.length === 0 ? <section
             className="p-3 w-[90%] mt-3 mb-3 h-[75vh] flex flex-col gap-2 overflow-auto">
             {repositories?.map((repository) => (
                 <RepositoryCard key={repository.id} name={repository.name} description={repository.description} url={repository.url} />
             ))}
         </section>
+            :
+            <section
+                className="p-3 w-[90%] mt-3 mb-3 h-[75vh] flex flex-col gap-2 overflow-auto">
+                {filteredRepositories?.map((repository) => (
+                    <RepositoryCard key={repository.id} name={repository.name} description={repository.description} url={repository.url} />
+                ))}
+            </section>}
+    </>
     )
 }
 
--- src/pages/ResultPage.tsx
+++ src/pages/ResultPage.tsx
@@ -1,10 +1,16 @@
 import { useLocation } from 'react-router-dom'
-import { useState, useEffect } from 'react'
+import { useState, useEffect, CSSProperties } from 'react'
 import fetchData from '../api/fetchData';
 import { FiGithub } from "react-icons/fi";
 import FilterBar from '../components/FilterBar';
 import Repositories from '../components/Repositories';
+import ClipLoader from "react-spinners/ClipLoader";
+import error404 from '../assets/404.jpg'
 
+const override: CSSProperties = {
+    display: "block",
+    margin: "auto",
+};
 interface User {
     login: string;
     name: string;
@@ -32,6 +38,7 @@ interface Repository {
 const ResultPage = () => {
     const [searchResults, setSearchResults] = useState<User | null>(null);
     const [filter, setFilter] = useState<string>('');
+    const [loading, setLoading] = useState(false);
     const location = useLocation();
     const searchQuery: string = location.state.name;
 
@@ -66,45 +73,68 @@ const ResultPage = () => {
         fetchUserData();
     }, [])
 
+    useEffect(() => {
+        setLoading(true);
+        setTimeout(() => {
+            setLoading(false);
+        }, 1000);
+    }, [])
+
     return (
-        <section className='grid grid-cols-4 p-4 gap-4 h-[100%] w-[100%] bg-gray-800 back"' style={{ backgroundImage: 'url(../../src/assets/background2.jpg)' }}>
-            <div
-                className='col-span-1 flex flex-col items-center mt-5'>
-                <img src={searchResults?.avatarUrl} alt='User Logo'
-                    className='xl:w-[250px] md:w-1/2 w-1/2 rounded-full m-1 border-4 border-slate-900' />
-                <p
-                    className='text-[#dfd9ff] font-medium lg:text-[30px] sm:text-[26px] xs:text-[20px] text-[16px] lg:leading-[40px] '
-                >{searchResults?.login}</p>
-                <p
-                    className='text-[#f1f1f1] font-medium lg:text-[18px] sm:text-[14px] xs:text-[10px] text-[12px] lg:leading-[30px]'
-                >{searchResults?.name}</p>
+        <>{loading ?
+            <ClipLoader
+                color={'black'}
+                loading={loading}
+                cssOverride={override}
+                size={150}
+            /> :
+            searchResults?.avatarUrl == null ?
+                <section
+                    className='flex flex-col align-center justify-center items-center'>
+                    <h1
+                        className='font-black text-white lg:text-[80px] sm:text-[60px] xs:text-[40px] text-[30px] lg:leading-[98px] mb-2'>404 - No user was found</h1>
+                    <img src={error404} alt='Error 404' />
+                </section>
+                :
+                <section className='grid grid-cols-4 p-4 gap-4 h-[100%] w-[100%] bg-gray-800 back"' style={{ backgroundImage: 'url(../../src/assets/background2.jpg)' }}>
+                    <div
+                        className='col-span-1 flex flex-col items-center mt-5'>
+                        <img src={searchResults?.avatarUrl} alt='User Logo'
+                            className='xl:w-[250px] md:w-1/2 w-1/2 rounded-full m-1 border-4 border-slate-900' />
+                        <p
+                            className='text-[#dfd9ff] font-medium lg:text-[30px] sm:text-[26px] xs:text-[20px] text-[16px] lg:leading-[40px] '
+                        >{searchResults?.login}</p>
+                        <p
+                            className='text-[#f1f1f1] font-medium lg:text-[18px] sm:text-[14px] xs:text-[10px] text-[12px] lg:leading-[30px]'
+                        >{searchResults?.name}</p>
 
-                <div
-                    className='flex flex-row gap-2 mt-4'>
-                    <p
-                        className='text-[#dfd9ff] text-[16px] font-bold'
-                    >{searchResults?.followers.totalCount}<span className='text-[#ffff] text-[14px] font-medium'> Followers</span>
-                    </p>
-                    <p
-                        className='text-[#dfd9ff] text-[16px] font-bold'
-                    >{searchResults?.following.totalCount}<span className='text-[#ffff] text-[14px] font-medium'> Following</span>
-                    </p>
-                </div>
-                <a
-                    href={searchResults?.url}
-                    target='_blank'
-                    className='flex flex-row gap-2 mt-10 align-center justify-center items-center text-[#ffff] text-[16px] font-bold h-[34px] border-2 p-4 bg-black hover:bg-gray-700 transition-all duration-500 rounded-md'>
-                    <span>Github Profile</span> <FiGithub />
-                </a>
+                        <div
+                            className='flex flex-row gap-2 mt-4'>
+                            <p
+                                className='text-[#dfd9ff] text-[16px] font-bold'
+                            >{searchResults?.followers.totalCount}<span className='text-[#ffff] text-[14px] font-medium'> Followers</span>
+                            </p>
+                            <p
+                                className='text-[#dfd9ff] text-[16px] font-bold'
+                            >{searchResults?.following.totalCount}<span className='text-[#ffff] text-[14px] font-medium'> Following</span>
+                            </p>
+                        </div>
+                        <a
+                            href={searchResults?.url}
+                            target='_blank'
+                            className='flex flex-row gap-2 mt-10 align-center justify-center items-center text-[#ffff] text-[16px] font-bold h-[34px] border-2 p-4 bg-black hover:bg-gray-700 transition-all duration-500 rounded-md'>
+                            <span>Github Profile</span> <FiGithub />
+                        </a>
 
 
-            </div>
-            <div
-                className='col-span-3 p-3 '>
-                <FilterBar setFilter={setFilter} />
-                <Repositories filter={filter} repositories={searchResults?.repositories.nodes} />
-            </div>
-        </section>
+                    </div>
+                    <div
+                        className='col-span-3 p-3 '>
+                        <FilterBar setFilter={setFilter} />
+                        <Repositories filter={filter} repositories={searchResults?.repositories.nodes} />
+                    </div>
+                </section>}
+        </>
     )
 }
 
